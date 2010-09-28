9d0fc0935012eaf37514568a3357b58c640dbc4f
Nicholas Avenell <nicholas@aquarionics.com>
2010-09-28 22:35:51 +0100

    Better detection of recents and a new flag for debug output

--- imports/steam.py
+++ imports/steam.py
@@ -5,6 +5,8 @@ import hashlib
 import os, time,sys,codecs
 import ConfigParser, MySQLdb, socket
 
+DEBUG = False;
+
 sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 
 basedir = os.path.dirname(os.path.abspath(sys.argv[0]))
@@ -30,6 +32,9 @@ user = sys.argv[1]
 
 s_sql = u'insert delayed ignore into lifestream (`id`, `type`, `systemid`, `title`, `date_created`, `image`, `url`, `source`) values (0, %s, %s, %s, NOW(), %s, %s, "steam");'
 
+if DEBUG:
+	print "Opening http://steamcommunity.com/id/%s/games?tab=recent&xml=1" % user;
+
 try:
 	gameslist_xml = urllib.urlopen("http://steamcommunity.com/id/%s/games?tab=recent&xml=1" % user)
 except IOError:
@@ -37,17 +42,43 @@ except IOError:
 	os._exit(5)
 
 games = minidom.parse(gameslist_xml);
-gameslist = games.getElementsByTagName('game')
-for game in gameslist[0:10]:
+
+gamesList = games.getElementsByTagName('game')
+
+maxGames   = 10
+thisGame   = 0
+foundGames = 0
+
+#for game in gameslist:
+while (foundGames < maxGames and thisGame != len(gamesList)):
+
+	# Get the current game
+	game = gamesList[thisGame]
+	
+	# Iterate Loop counter to get the next game next time
+	thisGame = thisGame + 1;
+
 	statspage = game.getElementsByTagName('statsLink')
 	gamename = game.getElementsByTagName('name')[0].firstChild.data
-	#print gamename
+	
+	if DEBUG:
+		print "% 3d % 3d %s" % (foundGames, thisGame, gamename)
+	
 	if len(statspage) == 0:
+		if DEBUG:
+			print "       + Skipping %s" % gamename	
 		continue;
+	else:
+		if DEBUG:
+			print "       + Keeping %s" % gamename	
+
+	# If we found a statspage, carry on. Iterate foundGames	
+
+	foundGames = foundGames + 1;
+
 
 	statspage = game.getElementsByTagName('statsLink')[0].firstChild.data
 	statspagexml = "%s?xml=1" % statspage
-	#print "%s %s" % (gamename, statspage)
 
 	try:
 		game = minidom.parse(urllib.urlopen(statspagexml))
@@ -65,7 +96,8 @@ for game in gameslist[0:10]:
 		name = achivement.getElementsByTagName('name')[0].firstChild.data
 		image = achivement.getElementsByTagName('iconClosed')[0].firstChild.data
 
-		#print name	
+		if DEBUG:
+			print "         %s" % name	
 
 		message = "%s &ndash; %s" % (gamename, name)
 	
