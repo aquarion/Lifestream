9afc4213c61d8b4bde3ac3ab6f41c6154387420a
Nicholas Avenell <nicholas@aquarionics.com>
2011-03-21 19:52:25 +0000

    Foursquare support

---
+++ imports/foursquare.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python
+import codecs, sys, os
+import ConfigParser, MySQLdb, socket
+
+import re
+
+from datetime import datetime
+
+import oauth2
+from twitter.oauth import write_token_file, read_token_file
+import json
+import httplib2
+
+from pprint import pprint
+
+sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+
+basedir = os.path.dirname(os.path.abspath(sys.argv[0]))
+config = ConfigParser.ConfigParser()
+config.readfp(open(basedir+'/../dbconfig.ini'))
+db = {}
+
+
+for item in config.items("database"):
+	db[item[0]] = item[1]
+
+dbcxn = MySQLdb.connect(user = db['username'], passwd = db['password'], db = db['database'], host = db['hostname'])
+cursor = dbcxn.cursor()
+
+if (len(sys.argv) < 3):
+	print "Usage: lifestreamit class username"
+	sys.exit(5)
+
+type = sys.argv[1]
+username = sys.argv[2]
+
+url = "http://foursquare.com/%s" % username
+
+s_sql = u'replace into lifestream (`type`, `systemid`, `title`, `date_created`, `url`, `source`, `image`) values (%s, %s, %s, %s, %s, %s, %s);'
+
+OAUTH_FILENAME = os.environ.get('HOME', '') + os.sep + '.foursquare_oauth_'+username
+CONSUMER_KEY = config.get("foursquare", "client_id")
+CONSUMER_SECRET = config.get("foursquare", "secret")
+
+if not os.path.exists(OAUTH_FILENAME):
+	print "You need to run foursquare_oauth.py to generate the oauth key"
+	sys.exit(5)
+
+
+oauth_token, oauth_token_secret = read_token_file(OAUTH_FILENAME)
+
+URL_BASE = "https://api.foursquare.com/v2/%%s?oauth_token=%s" % oauth_token
+web = httplib2.Http()
+
+ 
+response, content = web.request(URL_BASE% "users/self/checkins")
+data = json.loads(content)
+checkins = data['response']['checkins']['items']
+
+for location in checkins:
+    
+    source = "Foursquare";
+    if "isMayor" in location.keys() and location['isMayor']:
+        source = "Foursquare-Mayor"
+        
+    image = ""
+
+    source = re.sub(r'<[^>]*?>', '', source) 
+
+    if "venue" in location.keys():
+        message = location['venue']['name']
+        if len(location['venue']['categories']):
+            for category in location['venue']['categories']:
+                if "primary" in category.keys():
+                    image = category['icon']
+    else:
+        message = location['location']['name']
+        
+    epoch = location['createdAt']
+
+    utcdate = datetime.fromtimestamp(epoch).strftime("%Y-%m-%d %H:%M")
+    
+    id = location['id']
+
+    #print utcdate, message, image
+    cursor.execute(s_sql, (type, id, message, utcdate, url, source, image))
+
+response, content = web.request(URL_BASE% "users/self/badges")
+data = json.loads(content)
+badges = data['response']['badges']
+
+for badgeid, badge in badges.items():
+        
+    if len(badge['unlocks']) == 0:
+        continue
+    
+    source  = "Foursquare-Badge";
+    message = badge['name']
+    image   = badge['image']['prefix']+ "%s" % badge['image']['sizes'][1] +badge['image']['name']
+        
+    checkin = badge['unlocks'][0]['checkins'][0]
+    id      = checkin['id']
+    epoch   = checkin['createdAt']
+    utcdate = datetime.fromtimestamp(epoch).strftime("%Y-%m-%d %H:%M")
+    cursor.execute(s_sql, (type, id, message, utcdate, url, source, image))
+    #print utcdate, message, image
---
+++ imports/foursquare_oauth.py
@@ -0,0 +1,54 @@
+import json
+import pprint
+import os
+import sys
+import ConfigParser
+from httplib2 import Http
+
+OAUTH_BASE = "https://foursquare.com/oauth2/authenticate"
+
+CLIENT_ID = ""
+CLIENT_SECRET = ""
+CALLBACK_URL = ""
+
+FS_ACCESS_CODE = ""
+FS_ACCESS_TOKEN = ""
+
+if (len(sys.argv) < 2):
+        print "Usage: %s username" % sys.argv[0]
+        sys.exit(5)
+
+username = sys.argv[1]
+
+if len(sys.argv) == 3:
+	FS_ACCESS_CODE=sys.argv[2]
+
+basedir = os.path.dirname(os.path.abspath(sys.argv[0]))
+config = ConfigParser.ConfigParser()
+config.readfp(open(basedir+'/../dbconfig.ini'))
+
+OAUTH_FILENAME = os.environ.get('HOME', '') + os.sep + '.foursquare_oauth_'+username
+CLIENT_ID = config.get("foursquare", "client_id")
+CLIENT_SECRET = config.get("foursquare", "secret")
+CALLBACK_URL="www.github.com/aquarion/lifestream"
+
+
+print CLIENT_ID
+
+if not CLIENT_ID:
+  print "=== Register your Foursquare data here"
+  print "=== Copy back CLIENT_ID, CLIENT_SECRET and CALLBACK_URL here"
+  print "https://foursquare.com/oauth/"
+elif not FS_ACCESS_CODE:
+  print "=== Enter this URL in your browser."
+  print "=== Copy the CODE in the URL that results and run %s %s [code]" % (sys.argv[0], sys.argv[1])
+
+  print "https://foursquare.com/oauth2/authenticate?client_id=%s&response_type=code&redirect_uri=%s" % (CLIENT_ID, CALLBACK_URL)
+
+else:
+	url="https://foursquare.com/oauth2/access_token?client_id=%s&client_secret=%s&grant_type=authorization_code&redirect_uri=%s&code=%s" % (CLIENT_ID, CLIENT_SECRET, CALLBACK_URL, FS_ACCESS_CODE)
+	web = Http()
+	resp, content = web.request(url)
+	pprint.pprint(resp)
+	pprint.pprint(content)
+	
