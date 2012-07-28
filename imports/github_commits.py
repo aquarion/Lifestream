d681bd410a87f1a6f069f8a65008eaa243890b16
Nicholas Avenell <nicholas@aquarionics.com>
2012-07-29 00:06:40 +0100

    Move over to Github v3 support for commits

--- imports/github_commits.py
+++ imports/github_commits.py
@@ -2,61 +2,67 @@
 
 import lifestream
 
-import github
 import pytz
 import dateutil.parser
+import sys
 
-from urllib2 import URLError, HTTPError
+import urllib2
 
 dbcxn  = lifestream.getDatabaseConnection()
 cursor = lifestream.cursor(dbcxn)
 
-API_KEY    = lifestream.config.get("github", "key")
 USERNAME   = lifestream.config.get("github", "username")
+PASSWORD   = lifestream.config.get("github", "password")
 MAX_PAGES  = 2
 URL_PREFIX = "https://github.com"
 
-gh = github.GitHub(USERNAME, API_KEY)
 
 s_sql = u'replace into lifestream (`type`, `systemid`, `title`, `date_created`, `url`, `source`) values (%s, %s, %s, %s, %s, %s);'
 
 ls_type = "code"
+ls_source = "github"
 
-author = False
 
-for r in gh.repos.forUser(USERNAME):
-    ls_source = "github"
-    
-    p = 1
-    keep_going = True
-    
-    while keep_going:
-      try:
-      	commits = gh.commits.forBranch(USERNAME, r.name, page=p)
-      except HTTPError:
-      	commits = ()
-      if len(commits) == 0 or p == MAX_PAGES:
-        keep_going = False
-      else:
-        p += 1        
-        for c in commits:
-          if not hasattr(c.author, 'login'):
-            c.author = author
-            
-          if c.author.login.lower() == USERNAME.lower():
-            author = c.author
-            #print "%s %s" % (c.id[:7], c.message[:60].split("\n")[0])
-            #print c.id
-            if not hasattr(c, 'message'):
-              c.message = "Empty message"
-              
-            
-            c.message = "%s: %s" % (r.name, c.message)
-            
-            if len(c.message) > 250:
-		          c.message = c.message[0:250]
-		        
-            localdate = dateutil.parser.parse(c.authored_date)
-            utcdate = localdate.astimezone(pytz.utc).strftime("%Y-%m-%d %H:%M")
-            cursor.execute(s_sql, (ls_type, c.id, c.message, utcdate, URL_PREFIX+c.url, ls_source))
+import requests
+import simplejson as json
 
+
+def github_call(path, username, password, page = 1, perpage = 100):
+	gh_url = 'https://api.github.com/%s?page=%d&perpage=%d' % (path, page, perpage)
+	r = requests.get(gh_url, auth=(username, password))
+	if not r.status_code == 200:
+		print r.status_code
+		print r.url
+		print r.text
+		raise Exception
+	else:
+		return json.loads(r.text)
+
+repos = github_call("user/repos", USERNAME, PASSWORD)
+
+for repo in repos:
+	if repo['private']:
+		continue;
+		
+	commits = github_call("repos/%s/commits" % repo['full_name'], USERNAME, PASSWORD)
+	for commit in commits:
+		
+		if commit['author'] == None:
+			author = repo['owner']['login']
+		else:
+			author = commit['author']['login']
+		
+
+		if not USERNAME.lower() == author.lower():
+			#print "%s - Skipped" % commit['commit']['message']
+			continue
+		
+		message   = "%s: %s" % (repo['name'], commit['commit']['message'])
+		url       = commit['url']
+		localdate = dateutil.parser.parse(commit['commit']['author']['date'])
+		utcdate   = localdate.astimezone(pytz.utc).strftime("%Y-%m-%d %H:%M")
+		id        = commit['sha']
+		
+		#print message
+		cursor.execute(s_sql, (ls_type, id, message, utcdate, url, ls_source))
+		#print s_sql % (ls_type, id, message, utcdate, url, ls_source)
