e8315cc0c7a078eb2b9b59eb59d52758a5c2db3a
Nicholas Avenell <nicholas@aquarionics.com>
2013-05-16 22:44:27 +0100

    New flickr improt

--- imports/flickr.py
+++ imports/flickr.py
@@ -1,36 +1,73 @@
 #!/usr/bin/python
 
 import lifestream
-
-import feedparser
 import sys
-import dateutil.parser
+import flickrapi
 
+max_pages = False
+per_page  = 100
 
-if (len(sys.argv) < 3):
-	print "Usage: %s type ID" % sys.argv[0]
-	sys.exit(5)
+API_KEY       = lifestream.config.get("flickr", "api_key")
+API_SECRET    = lifestream.config.get("flickr", "api_secret")
+FLICKRID      = lifestream.config.get("flickr", "account")
+FLICKRNAME    = lifestream.config.get("flickr", "account_name")
 
-url = "http://api.flickr.com/services/feeds/photos_public.gne?id=%s&lang=en-us&format=atom" % sys.argv[2]
+DEBUG = False
 
 dbcxn  = lifestream.getDatabaseConnection()
 cursor = lifestream.cursor(dbcxn)
 
-fp = feedparser.parse(url)
+## Only search from the most recent result
+sql = 'select date_created from lifestream where type = "flickr" order by date_created desc limit 1 ';
+cursor.execute(sql)
+res = cursor.fetchone()
 
-type = sys.argv[1]
+if res:
+	since = res[0]
+else:
+	since = 0
+	
+flickr = flickrapi.FlickrAPI(API_KEY)
+photos_xml = flickr.photos_search(user_id=FLICKRID, per_page=per_page, page=1, min_taken_date=since)
+#sets = flickr.photosets_getList(user_id='73509078@N00')
+
+print "Since %s" % since
+
+pages = int(photos_xml.find('photos').attrib['pages'])
+
+print pages
+
+if pages == 0:
+	if DEBUG:
+		print "No photos found"
+	sys.exit(0);
+
+if max_pages and pages > max_pages:
+	pages = max_pages;
 
 s_sql = u'replace into lifestream (`type`, `systemid`, `title`, `url`, `date_created`, `source`, `image`) values (%s, %s, %s, %s, %s, "", %s);'
+s_url = 'http://farm%s.staticflickr.com/%s/%s_%s_%s.jpg' #% (farm, server, id, secret, size)
 
-for i in range(len(fp['entries'])):
-	o_item = fp['entries'][i]
-	id = o_item['guid']
-	result = dateutil.parser.parse( o_item['published'])
-	message = o_item['title'].replace('"', '\\"');
+size_code = "z"
+type = "flickr"
 
-	image = ""
-	for link in o_item['links']:
-		if link['rel'] == u'enclosure':
-			image = link['href']
 
-	cursor.execute(s_sql, (type, id, message, o_item['links'][0]['href'],  result, image))
+for page in range(1,pages+1):
+	if DEBUG:
+		print "Page %d of %d" % (page, pages)
+	photos_xml = flickr.photos_search(user_id=FLICKRID, per_page=per_page, page=page, min_taken_date =since)
+	photos = photos_xml.find("photos").findall("photo")
+	for photo in photos:
+		image = s_url % (photo.attrib['farm'], photo.attrib['server'], photo.attrib['id'], photo.attrib['secret'], size_code);
+		
+		info = flickr.photos_getinfo(photo_id=photo.attrib['id'], secret=photo.attrib['secret'])
+		
+		page_url = info.find("photo").find("urls").find("url").text
+		title = info.find("photo").find("title").text
+		id = photo.attrib['id']
+		date_taken = info.find("photo").find("dates").attrib['taken']
+		
+		if DEBUG:
+			print "     %s %s" % (date_taken, title)
+		
+		cursor.execute(s_sql, (type, id, title, page_url, date_taken, image))
