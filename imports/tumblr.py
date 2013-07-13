cb4154d47f944b1f301fce8e61e8739d399fcdba
Nicholas Avenell <nicholas@aquarionics.com>
2013-07-13 10:54:08 +0100

    Use tumblr api for import

--- imports/tumblr.py
+++ imports/tumblr.py
@@ -2,33 +2,114 @@
 
 import lifestream
 
-import feedparser
-import sys
+from pprint import pprint
+
+import sys, urlparse, cPickle as pickle
+import simplejson
 from datetime import datetime
 from time import mktime
 
-if (len(sys.argv) < 3):
-	print "Usage: %s type url" % sys.argv[0]
-	sys.exit(5)
-
-type = sys.argv[1]
-url = sys.argv[2]
+from pytumblr import TumblrRestClient
+import oauth2 as oauth
 
 dbcxn = lifestream.getDatabaseConnection()
 cursor = lifestream.cursor(dbcxn)
 
-fp = feedparser.parse(url)
-
+OAUTH_TUMBLR  = lifestream.config.get("tumblr", "secrets_file")
 
 s_sql = u'replace into lifestream (`type`, `systemid`, `title`, `url`, `date_created`, `source`, `image`) values (%s, %s, %s, %s, %s, "", "");'
 
-for i in range(len(fp['entries'])):
-	o_item   = fp['entries'][i]
-	id       = o_item['guid']
-	dt       = datetime.fromtimestamp(mktime(o_item['updated_parsed']))
-	updated  = dt.strftime("%Y-%m-%d %H:%M")
-	if "summary" in o_item.keys():
-		words = (o_item['summary'][:512] + '..') if len(o_item['summary']) > 512 else o_item['summary']
-	else:
-		words = o_item['title']
-	cursor.execute(s_sql, (type, id, words, o_item['links'][0]['href'],  updated))
+
+def tumblrAuth(config, OAUTH_TUMBLR):
+	consumer_key    = config.get("tumblr", "consumer_key")
+	consumer_secret = config.get("tumblr", "secret_key")
+
+	request_token_url = 'http://www.tumblr.com/oauth/request_token'
+	access_token_url = 'http://www.tumblr.com/oauth/access_token'
+	authorize_url = 'http://www.tumblr.com/oauth/authorize'
+
+	try:
+		f = open(OAUTH_TUMBLR, "rb")
+		oauth_token = pickle.load(f)
+		f.close();
+	except:
+		print "Couldn't open %s, reloading..." % OAUTH_TUMBLR
+		oauth_token = False
+
+	if(not oauth_token):
+		consumer = oauth.Consumer(consumer_key, consumer_secret)
+		client   = oauth.Client(consumer)
+		resp, content = client.request(request_token_url, "GET")
+		if resp['status'] != '200':
+		    raise Exception("Invalid response %s." % resp['status'])
+
+		request_token = dict(urlparse.parse_qsl(content))
+		print "Go to the following link in your browser:"
+		print "%s?oauth_token=%s" % (authorize_url, request_token['oauth_token'])
+		print
+
+		accepted = 'n'
+		while accepted.lower() == 'n':
+		    accepted = raw_input('Have you authorized me? (y/n) ')
+		oauth_verifier = raw_input('What is the PIN? ')
+
+		token = oauth.Token(request_token['oauth_token'], request_token['oauth_token_secret'])
+		token.set_verifier(oauth_verifier)
+		client = oauth.Client(consumer, token)
+
+
+		resp, content = client.request(access_token_url, "POST")
+		oauth_token = dict(urlparse.parse_qsl(content))
+		print resp
+
+		print oauth_token
+		print "Access key:", oauth_token['oauth_token']
+		print "Access Secret:", oauth_token['oauth_token_secret']
+
+		f = open(OAUTH_TUMBLR, "w")
+		pickle.dump(oauth_token, f)
+		f.close();
+		
+	return TumblrRestClient(consumer_key, consumer_secret, oauth_token['oauth_token'], oauth_token['oauth_token_secret'])
+
+tumblr = tumblrAuth(lifestream.config, OAUTH_TUMBLR)
+
+s_sql = u'replace into lifestream (`type`, `systemid`, `title`, `url`, `date_created`, `source`, `image`) values (%s, %s, %s, %s, %s, "tumblr", %s);'
+
+
+blogs = lifestream.config.get("tumblr","blogs").split(",")
+
+for blog in blogs:
+	details = tumblr.posts(blog)
+	startat = 0.0;
+	max_posts = details['blog']['posts']
+
+
+	# while startat < max_posts:
+	# 	details = tumblr.posts(blog, offset=startat, limit=20)
+	# 	startat += 20;
+
+	posts = details['posts'];
+
+	print "%s %d/%d %.2f%%" % (blog, startat,max_posts, (startat/max_posts)*100.0);
+
+	for post in posts:
+		id   = post['id'];
+		type = post['type']
+		url  = post['post_url']
+		image = False
+
+		if post.has_key("title") and post['title']:
+			title = post['title']
+		else:
+			title = "Tumblr %s" % type
+
+		if type == "quote":
+			title = post['text']
+
+		if type == "photo":
+			image = post['photos'][0]['original_size']['url']
+
+		#cursor.execute(s_sql, (type, id, title, url,  post['date'], image))
+
+		print url, title
