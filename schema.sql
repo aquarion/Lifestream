0bc5aded6b07721c49b336a6fb887b0f0cc6a4a9
Nicholas Avenell <nicholas@aquarionics.com>
2015-04-08 18:04:10 +0100

    Update schema file

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.11
+-- MySQL dump 10.13  Distrib 5.5.41, for debian-linux-gnu (x86_64)
 --
--- Host: localhost    Database: klind
+-- Host: localhost    Database: aquarion_lifestream
 -- ------------------------------------------------------
--- Server version	5.0.51a-24+lenny5-log
+-- Server version	5.5.41-0+wheezy1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -20,42 +20,62 @@
 --
 
 DROP TABLE IF EXISTS `lifestream`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `lifestream` (
-  `id` int(11) default NULL,
-  `type` varchar(15) collate utf8_unicode_ci NOT NULL default '',
-  `systemid` varchar(128) collate utf8_unicode_ci NOT NULL default '',
-  `title` varchar(2047) collate utf8_unicode_ci default NULL,
-  `date_created` datetime default NULL,
-  `image` varchar(255) collate utf8_unicode_ci NOT NULL default '',
-  `url` varchar(511) collate utf8_unicode_ci NOT NULL,
-  `source` varchar(255) collate utf8_unicode_ci NOT NULL default '',
-  PRIMARY KEY  (`systemid`,`type`)
+  `id` int(11) DEFAULT NULL,
+  `type` varchar(15) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `systemid` varchar(128) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `title` varchar(2047) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `date_created` datetime DEFAULT NULL,
+  `image` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `url` varchar(511) COLLATE utf8_unicode_ci NOT NULL,
+  `source` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `date_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `subtype` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `fulldata_json` mediumtext CHARACTER SET utf32 COLLATE utf32_unicode_ci,
+  PRIMARY KEY (`systemid`,`type`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-SET character_set_client = @saved_cs_client;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `lifestream_locations`
 --
 
 DROP TABLE IF EXISTS `lifestream_locations`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `lifestream_locations` (
   `id` bigint(20) unsigned NOT NULL,
-  `source` varchar(255) collate utf8_unicode_ci NOT NULL,
-  `lat` double default NULL,
-  `long` double default NULL,
-  `lat_vague` double default NULL,
-  `long_vague` double default NULL,
+  `source` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `lat` double DEFAULT NULL,
+  `long` double DEFAULT NULL,
+  `alt` int(11) DEFAULT NULL,
+  `alt_vague` int(11) DEFAULT NULL,
+  `lat_vague` double DEFAULT NULL,
+  `long_vague` double DEFAULT NULL,
   `timestamp` datetime NOT NULL,
   `accuracy` int(11) NOT NULL,
-  `title` varchar(255) collate utf8_unicode_ci default NULL,
-  `icon` varchar(255) collate utf8_unicode_ci default NULL,
-  PRIMARY KEY  (`id`,`source`)
+  `title` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `icon` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`,`source`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-SET character_set_client = @saved_cs_client;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `lifestream_stats`
+--
+
+DROP TABLE IF EXISTS `lifestream_stats`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lifestream_stats` (
+  `date` date NOT NULL,
+  `statistic` char(31) COLLATE utf8_unicode_ci NOT NULL,
+  `number` int(11) NOT NULL,
+  PRIMARY KEY (`date`,`statistic`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -66,4 +86,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-04-05  1:05:32
+-- Dump completed on 2015-04-08 18:02:17
