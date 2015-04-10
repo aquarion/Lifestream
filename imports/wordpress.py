9db73c7d241e4601816d63b65f98b3148925cbda
Nicholas Avenell <nicholas@aquarionics.com>
2015-04-10 08:51:16 +0100

    Add logging to wordpress

--- imports/wordpress.py
+++ imports/wordpress.py
@@ -3,7 +3,6 @@
 
 import lifestream
 
-import feedparser
 import sys
 import codecs, locale
 from datetime import datetime
@@ -16,33 +15,34 @@ from wordpress_xmlrpc import Client, WordPressPost
 from wordpress_xmlrpc.methods.posts import GetPosts, NewPost
 from wordpress_xmlrpc.methods.users import GetUserInfo
 
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('site', type=str,
+lifestream.arguments.add_argument('site', type=str,
                    help='Site, as defined in config.ini')
-parser.add_argument('--all', action="store_true",
+lifestream.arguments.add_argument('--all', action="store_true",
                    help='Fetch all posts?',
                    dest='all_pages')
-parser.add_argument('--max_pages', type=int, 
+lifestream.arguments.add_argument('--max_pages', type=int, 
                    help='How many pages to fix (overriden by --all)',
                    default=1,
                    required=False)
 
-args = parser.parse_args()
+args = lifestream.arguments.parse_args()
 
 source = args.site
 type = "wordpress"
 
 Lifestream = lifestream.Lifestream()
 
+logger = logging.getLogger('WP/%s' % site)
+
 try:
     url    = lifestream.config.get("wordpress:%s" % source, "url")
     user   = lifestream.config.get("wordpress:%s" % source, "username")
     passwd = lifestream.config.get("wordpress:%s" % source, "password")
 except ConfigParser.NoSectionError:
-    print "No [wordpress:%s] section found in config" % source
+    logger.error("No [wordpress:%s] section found in config" % source)
     sys.exit(5)
 except ConfigParser.NoOptionError as e:
-    print e.message
+    logger.error(e.message)
     sys.exit(5)
 
 
@@ -81,7 +81,7 @@ while keep_going:
             # fulldata_json=post.struct,
             type=type)
 
-        print "%s: %s" % (post.date.strftime("%Y-%m-%d"), title)
+        logger.info("%s: %s" % (post.date.strftime("%Y-%m-%d"), title))
 
     if not len(posts):
         keep_going = False
@@ -90,24 +90,7 @@ while keep_going:
     if this_page >= args.max_pages and not args.all_pages:
         keep_going = False
     elif not args.all_pages:
-        print "Page %d of max %d" % (this_page, args.max_pages)
+        logger.info("Page %d of max %d" % (this_page, args.max_pages))
     else:
-        print "Next Page..."
+        logger.info("Next Page...")
 
-
-# import ipdb
-# ipdb.set_trace()
-
-# for i in range(len(fp['entries'])):
-#     o_item = fp['entries'][i]
-#     id = o_item['guid']
-#     dt = datetime.fromtimestamp(mktime(o_item['updated_parsed']))
-#     updated = dt.strftime("%Y-%m-%d %H:%M")
-
-#     Lifestream.add_entry(
-#         type=type,
-#         id=id,
-#         title=o_item['title'],
-#         source=type,
-#         date=updated,
-#         url=o_item['links'][0]['href'])
