bd326eea9be343d46ae2a6cae1a875e0a5b0d043
Nicholas Avenell <nicholas@istic.net>
2016-02-14 19:31:27 +0000

    [Foursquare] 4SQ has deprecated badges, so we no longer track them

--- imports/foursquare.py
+++ imports/foursquare.py
@@ -54,18 +54,19 @@ Lifestream = lifestream.Lifestream()
 l_sql = u'replace into lifestream_locations (`id`, `source`, `lat`, `long`, `lat_vague`, `long_vague`, `timestamp`, `accuracy`, `title`, `icon`) values (%s, "foursquare", %s, %s, %s, %s, %s, 1, %s, %s);'
 
 
-URL_BASE = "https://api.foursquare.com/v2/%%s?oauth_token=%s" % oauth_token
-
+URL_BASE = "https://api.foursquare.com/v2/%s" 
 # Get the data
 
-payload = {'v': "20140226"}
+payload = {
+	'v': "20140226",
+	'oauth_token' : oauth_token
+}
 
 r = requests.get(URL_BASE % "users/self/checkins", params=payload)
 
 data = r.json()
 
 checkins = data['response']['checkins']['items']
-
 
 if 'checkins' in data['response'].keys():
     for location in checkins:
@@ -121,42 +122,4 @@ if 'checkins' in data['response'].keys():
              utcdate,
              location['venue']['name'],
              image))
-
-
-r = requests.get(URL_BASE % "users/self/badges", params=payload)
-
-data = r.json()
-
-badges = data['response']['badges']
-
-for badgeid, badge in badges.items():
-
-    if len(badge['unlocks']) == 0:
-        continue
-
-    source = "Foursquare-Badge"
-    message = badge['name']
-    image = badge['image']['prefix'] + \
-        "%s" % badge['image']['sizes'][1] + badge['image']['name']
-
-    checkin = badge['unlocks'][0]['checkins'][0]
-    id = checkin['id']
-
-    epoch = checkin['createdAt']
-    utctime = datetime.utcfromtimestamp(epoch)
-    utcdate = utctime.strftime("%Y-%m-%d %H:%M")
-
-    logger.info("Badge %s" % (message))
-
-    Lifestream.add_entry(
-        type,
-        id,
-        message,
-        source,
-        utcdate,
-        url=url,
-        image=image,
-        fulldata_json=badge)
-
-dbcxn.close()
 
