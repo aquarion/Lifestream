589f47d3fefea3885f3ef13089b7db11bc8878fb
Nicholas Avenell <nicholas@istic.net>
2017-08-26 14:01:29 +0100

    Further moves dev

--- imports/dev_moves.py
+++ imports/dev_moves.py
@@ -18,6 +18,7 @@ import pickle
 from pprint import pprint
 import urlparse
 from datetime import timedelta
+import CodeFetcher9000
 
 # Libraries
 import facebook
@@ -52,7 +53,10 @@ OAUTH_FILENAME = "%s/moves.oauth" % (
 APP_KEY = lifestream.config.get("moves", "key")
 APP_SECRET = lifestream.config.get("moves", "secret")
 
+
+   #authenticate(OAUTH_FILENAME, appid, secret, force_reauth=False):
 def authenticate(OAUTH_FILENAME, appid, secret, force_reauth=False):
+
     scope = "activity+location"
     request_token_url = 'https://api.moves-app.com/oauth/v1/authorize?response_type=code&client_id=%s&scope=%s' % (appid, scope)
 
@@ -67,58 +71,145 @@ def authenticate(OAUTH_FILENAME, appid, secret, force_reauth=False):
     else:
         oauth_token = False
 
-    if(not oauth_token):
-        print "Go to the following link in your browser:"
-        print request_token_url
-        print
 
-        accepted = 'n'
-        while accepted.lower() == 'n':
-            accepted = raw_input('Have you authorized me? (y/n) ')
-        access_key = raw_input('What is the access code? ')
+    try:
+        CodeFetcher9000.are_we_working()
+        redirect_uri=CodeFetcher9000.get_url()
+        UseCodeFetcher = True
+    except CodeFetcher9000.WeSayNotToday:
+        try:
+            redirect_uri='{}/keyback/wow.py'.format(lifestream.config.get("dayze", "base")),
+            UseCodeFetcher = False
+        except ConfigParser.Error:
+            logger.error("Dayze base not configured")
+            print "To catch an OAuth request, you need either CodeFetcher9000 or Dayze configured in config.ini"
+            sys.exit(32)
 
-        logger.debug(access_key)
-        print "Access key:", access_key
+    if oauth_token:
 
-	extend_token_url = "https://api.moves-app.com/oauth/v1/access_token?grant_type=authorization_code&code=%s&client_id=%s&client_secret=%s" % (access_key, appid, secret)
-	extend_token = requests.post(extend_token_url)
-	oauth_token = extend_token.json()
+        expiration_date =  oauth_token['expire_dt']
+        if datetime.now() > expiration_date:
+            print "Token has expired!"
 
-	delta = timedelta(seconds=int(oauth_token['expires_in']))
-	oauth_token['expire_dt'] = datetime.now() + delta;
+        delta = expiration_date - datetime.now()
+
+        if delta.days <= 7:
+            print "Token will expire in {} days!".format(delta.days)
+
+        return oauth_token
+    
+    # Step 2: Redirect to the provider. Since this is a CLI script we do not 
+    # redirect. In a web application you would redirect the user to the URL
+    # below.
+
+    print "Go to the following link in your browser:"
+    print request_token_url
+    print 
+
+    if UseCodeFetcher:
+        oauth_redirect = CodeFetcher9000.get_code("code")
+        access_key = oauth_redirect['code'][0]
+    else:
+        print "If you configure CodeFetcher9000, this is a lot easier."
+        print " - "
+        access_key = raw_input('What is the PIN? ')
+    
+    extend_token_url = "https://api.moves-app.com/oauth/v1/access_token?grant_type=authorization_code&code=%s&client_id=%s&client_secret=%s" % (access_key, appid, secret)
+    extend_token = requests.post(extend_token_url)
+    oauth_token = extend_token.json()
+
+    delta = timedelta(seconds=int(oauth_token['expires_in']))
+    oauth_token['expire_dt'] = datetime.now() + delta;
         
-	f = open(OAUTH_FILENAME, "w")
-        pickle.dump(oauth_token, f)
-        f.close()
+    f = open(OAUTH_FILENAME, "w")
+    pickle.dump(oauth_token, f)
+    f.close()
 
     return oauth_token
+
+
+# def old_authenticate(OAUTH_FILENAME, appid, secret, force_reauth=False):
+#     scope = "activity+location"
+#     request_token_url = 'https://api.moves-app.com/oauth/v1/authorize?response_type=code&client_id=%s&scope=%s' % (appid, scope)
+
+#     if not force_reauth:
+#         try:
+#             f = open(OAUTH_FILENAME, "rb")
+#             oauth_token = pickle.load(f)
+#             f.close()
+#         except:
+#             logger.error("Couldn't open %s, reloading..." % OAUTH_FILENAME)
+#             oauth_token = False
+#     else:
+#         oauth_token = False
+
+#     if(not oauth_token):
+#         print "Go to the following link in your browser:"
+#         print request_token_url
+#         print
+
+#         accepted = 'n'
+#         while accepted.lower() == 'n':
+#             accepted = raw_input('Have you authorized me? (y/n) ')
+#         access_key = raw_input('What is the access code? ')
+
+#         logger.debug(access_key)
+#         print "Access key:", access_key
+
+#     extend_token_url = "https://api.moves-app.com/oauth/v1/access_token?grant_type=authorization_code&code=%s&client_id=%s&client_secret=%s" % (access_key, appid, secret)
+#     extend_token = requests.post(extend_token_url)
+#     oauth_token = extend_token.json()
+
+#     delta = timedelta(seconds=int(oauth_token['expires_in']))
+#     oauth_token['expire_dt'] = datetime.now() + delta;
+        
+#     f = open(OAUTH_FILENAME, "w")
+#         pickle.dump(oauth_token, f)
+#         f.close()
+
+#     return oauth_token
 
 
 credentials = authenticate(OAUTH_FILENAME, APP_KEY, APP_SECRET, args.reauth)
 
 
 if datetime.now() > credentials['expire_dt']:
-	logger.error("Token has expired!")
+    logger.error("Token has expired!")
 
 delta = credentials['expire_dt'] - datetime.now()
 
 if delta.days <= 7:
-	logger.warning("Token will expire in {} days!".format(delta.days))
+    logger.warning("Token will expire in {} days!".format(delta.days))
 else:
     logger.info("Token will expire in {} days!".format(delta.days))
 
 
 payload = { 
-	'access_token' : credentials['access_token'],
-	'pastDays'     : 7,
-	'trackPoints'  : "true"
-	}
+    'access_token' : credentials['access_token'],
+    'pastDays'     : 7,
+    'trackPoints'  : "true"
+    }
 
 BASEURL = "https://api.moves-app.com/api/1.1"
 
 url = BASEURL + "/user/storyline/daily"
 
-profile = requests.get(url, params=payload);
+profile = requests.get(url, params=payload).json();
 
-pprint(profile.json())
-
+for day in profile:
+    print '----', day['date']
+    for segment in day['segments']:
+        # print "Segment"
+        if 'activities' in segment:
+            for activity in segment['activities']:
+                print activity['group'], activity['distance']
+        if 'place' in segment:
+            place = segment['place']
+            pprint(place)
+            print "https://www.google.com/maps/preview/@%f,%f,15z " % (place['location']['lat'], segment['place']['location']['lon'])
+            #sys.exit(5)
+    print "Summary"
+    # pprint(day['summary']);
+    for activity in day['summary']:
+        print "%sm %s" % (activity['distance'], activity['activity'])
+    # pprint(day)
