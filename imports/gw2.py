f01fc46b0150ef0852f21b86b6b42b3aeb70639c
Nicholas Avenell <nicholas@istic.net>
2023-09-13 09:11:54 +0100

    ➕ Rewrite to chunk out large data requests

--- imports/gw2.py
+++ imports/gw2.py
@@ -3,9 +3,11 @@
 from datetime import datetime
 import logging
 import requests
+import sys
 
 # Libraries
 from guildwars2api.v2 import GuildWars2API
+from guildwars2api.base import GuildWars2APIError
 
 # Local
 import lifestream
@@ -22,33 +24,69 @@ api = GuildWars2API(
     user_agent="Lifestream <nicholas@istic.net>",
     api_key=APIKEY)
 
-my_achivements = api.account_achievements.get()
 
-fetch_list = []
 
-achivements_library = {}
+@Lifestream.cache_this('gw2.categories', 86400)
+def get_categories():
+    category_list = requests.get(
+        "https://api.guildwars2.com/v2/achievements/categories/").json()
 
-for achivement in my_achivements:
-    if achivement['done']:
-        ident = achivement['id']
-        fetch_list.append(ident)
-        achivements_library[ident] = {}
-        achivements_library[ident]['progress'] = achivement
+    category_fetch = []
 
-response = api.achievements.get(ids=fetch_list)
+    step = 200
+    for i in range(0, len(category_list), step):
+        logger.debug("Fetching Category {} to {} of {}".format(i, i + step, len(category_list)))
+        
+        category_data = {"ids": ','.join(str(x) for x in category_list[i:i+step])}
+        category_request = requests.get(
+        "https://api.guildwars2.com/v2/achievements/categories",
+        data=category_data).json()
+        category_fetch += category_request
 
-for achivement in response:
-    ident = achivement['id']
-    achivements_library[ident]['info'] = achivement
+    return category_fetch
 
-category_list = requests.get(
-    "https://api.guildwars2.com/v2/achievements/categories/").json()
-category_data = {"ids": ','.join(str(x) for x in category_list)}
-category_fetch = requests.get(
-    "https://api.guildwars2.com/v2/achievements/categories",
-    data=category_data).json()
 
-for category in category_fetch:
+def get_all_my_achivements(api):
+    my_achivements = api.account_achievements.get()
+
+    fetch_list = []
+
+    achivements_library = {}
+
+    for achivement in my_achivements:
+        if achivement['done']:
+            ident = achivement['id']
+            fetch_list.append(ident)
+            achivements_library[ident] = {}
+            achivements_library[ident]['progress'] = achivement
+
+
+    # Chunk 
+
+    logger.debug("Fetch list is {} long".format(len(fetch_list)))
+
+    ach_index = 0
+    ach_pagesize = 100
+    list_size = len(fetch_list)
+
+    for ach_index in range(0, list_size, ach_pagesize):
+        fetch_chunk = fetch_list[ach_index:ach_index+ach_pagesize]
+        logger.debug("Fetching Achivement {} to {} of {}".format(ach_index, ach_index + ach_pagesize, len(fetch_chunk)))
+
+        response = api.achievements.get(ids=fetch_chunk)
+
+        for achivement in response:
+            ident = achivement['id']
+            achivements_library[ident]['info'] = achivement
+
+    return achivements_library
+
+
+
+achivements_library = get_all_my_achivements(api)
+
+
+for category in get_categories():
     for achivement_id in category['achievements']:
         if achivement_id in achivements_library:
             achivements_library[achivement_id]['category'] = category
@@ -70,6 +108,8 @@ for ident, achivement in achivements_library.items():
 
     text = achivement['info']['name'] + " &ndash; " + \
         achivement['info']['requirement']
+    
+    logger.info(text)
 
 #     id = hashlib.md5()
 # id.update(text)
