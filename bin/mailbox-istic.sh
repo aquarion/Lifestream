b57ec0e1c1449cc8a057a5e046da9c3f3dfb0cb8
Nicholas Avenell <nicholas.avenell@iceye.com>
2025-05-04 21:48:09 +0100

    Refactor shell scripts for consistent syntax and remove obsolete mailbox-siftware.sh

--- bin/mailbox-istic.sh
+++ bin/mailbox-istic.sh
@@ -9,7 +9,7 @@ LOCKDIR=/tmp
 # Adapted by Nicholas Avenell <nicholas@aquarionics.com> for lifestream.
 
 # lock file creation and removal
-LOCKFILE=$LOCKDIR/`basename $0`.lock
+LOCKFILE=$LOCKDIR/$(basename $0).lock
 [ -f $LOCKFILE ] && echo $LOCKFILE exists && exit 0
 trap "{ rm -f $LOCKFILE; exit 255; }" 2
 trap "{ rm -f $LOCKFILE; exit 255; }" 9
@@ -18,12 +18,11 @@ trap "{ rm -f $LOCKFILE; exit 0; }" EXIT
 touch $LOCKFILE
 
 export DIRENV_LOG_FORMAT=""
-DIRNAME=`dirname $0`/../
+DIRNAME=$(dirname $0)/../
 cd $DIRNAME
 eval "$(direnv export bash)"
 
 python imports/mailbox-stats.py istic-email-recieved /home/aquarion/mail-archive/istic-backup.mbox
-
 
 # IMPORTANT: Run this first in a terminal or ssh session to go though the oauth keys
 # before you add it to cron.
