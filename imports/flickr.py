6c27e1ecf5ec604a9531809d33847a61bff5b186
Nicholas Avenell <nicholas@learningonscreen.ac.uk>
2026-03-04 11:28:09 +0000

    Refactor importers and add main function for better structure; enhance logging and error handling across modules

--- imports/flickr.py
+++ imports/flickr.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+"""Flickr photo importer for Lifestream."""
 
 # Python
 import logging
@@ -12,96 +13,89 @@ import lifestream
 from lifestream.db import EntryStore
 from lifestream.db import get_connection, get_cursor
 
-logging.root.handlers.pop(0)  # remove Flickrapi's log handler
-
-max_pages = False
-per_page = 100
-
-API_KEY = lifestream.config.get("flickr", "api_key")
-API_SECRET = lifestream.config.get("flickr", "api_secret")
-FLICKRID = lifestream.config.get("flickr", "account")
-FLICKRNAME = lifestream.config.get("flickr", "account_name")
-
-DEBUG = False
-dbcxn = get_connection()
-cursor = get_cursor(dbcxn)
-
-
-entry_store = EntryStore()
+# Remove Flickrapi's log handler
+logging.root.handlers.pop(0)
 
 logger = logging.getLogger("Flickr")
-args = lifestream.parse_args()
 
-# Only search from the most recent result
-sql = 'select date_created from lifestream where type = "flickr" order by date_created desc limit 1 '
-cursor.execute(sql)
-res = cursor.fetchone()
-
-if res:
-    since = res[0]
-else:
-    since = 0
-
-flickr = flickrapi.FlickrAPI(API_KEY)
-photos_xml = flickr.photos_search(
-    user_id=FLICKRID, per_page=per_page, page=1, min_taken_date=since
-)
-# sets = flickr.photosets_getList(user_id='73509078@N00')
+# Static URL template: farm, server, id, secret, size
+STATIC_URL_TEMPLATE = "http://farm%s.staticflickr.com/%s/%s_%s_%s.jpg"
+MAX_PAGES = False
+PER_PAGE = 100
+SIZE_CODE = "z"
 
 
-pages = int(photos_xml.find("photos").attrib["pages"])
+def main():
+    """Import photos from Flickr."""
+    args = lifestream.parse_args()
 
-logger.info("Since %s" % since)
+    api_key = lifestream.config.get("flickr", "api_key")
+    flickr_id = lifestream.config.get("flickr", "account")
 
-if pages == 0:
-    logger.info("Nothing found")
-    sys.exit(0)
+    dbcxn = get_connection()
+    cursor = get_cursor(dbcxn)
+    entry_store = EntryStore()
 
-if max_pages and pages > max_pages:
-    pages = max_pages
+    # Only search from the most recent result
+    sql = 'select date_created from lifestream where type = "flickr" order by date_created desc limit 1'
+    cursor.execute(sql)
+    res = cursor.fetchone()
+    since = res[0] if res else 0
 
-
-# % (farm, server, id, secret, size)
-s_url = "http://farm%s.staticflickr.com/%s/%s_%s_%s.jpg"
-
-size_code = "z"
-type = "flickr"
-
-
-for page in range(1, pages + 1):
-    logger.info("Page %d of %d" % (page, pages))
+    flickr = flickrapi.FlickrAPI(api_key)
     photos_xml = flickr.photos_search(
-        user_id=FLICKRID, per_page=per_page, page=page, min_taken_date=since
+        user_id=flickr_id, per_page=PER_PAGE, page=1, min_taken_date=since
     )
-    photos = photos_xml.find("photos").findall("photo")
-    for photo in photos:
-        image = s_url % (
-            photo.attrib["farm"],
-            photo.attrib["server"],
-            photo.attrib["id"],
-            photo.attrib["secret"],
-            size_code,
+
+    pages = int(photos_xml.find("photos").attrib["pages"])
+    logger.info("Since %s", since)
+
+    if pages == 0:
+        logger.info("Nothing found")
+        dbcxn.close()
+        sys.exit(0)
+
+    if MAX_PAGES and pages > MAX_PAGES:
+        pages = MAX_PAGES
+
+    for page in range(1, pages + 1):
+        logger.info("Page %d of %d", page, pages)
+        photos_xml = flickr.photos_search(
+            user_id=flickr_id, per_page=PER_PAGE, page=page, min_taken_date=since
         )
+        photos = photos_xml.find("photos").findall("photo")
+        for photo in photos:
+            image = STATIC_URL_TEMPLATE % (
+                photo.attrib["farm"],
+                photo.attrib["server"],
+                photo.attrib["id"],
+                photo.attrib["secret"],
+                SIZE_CODE,
+            )
 
-        info = flickr.photos_getinfo(
-            photo_id=photo.attrib["id"], secret=photo.attrib["secret"]
-        )
+            info = flickr.photos_getinfo(
+                photo_id=photo.attrib["id"], secret=photo.attrib["secret"]
+            )
 
-        page_url = info.find("photo").find("urls").find("url").text
-        title = info.find("photo").find("title").text
-        id = photo.attrib["id"]
-        date_taken = info.find("photo").find("dates").attrib["taken"]
+            page_url = info.find("photo").find("urls").find("url").text
+            title = info.find("photo").find("title").text
+            photo_id = photo.attrib["id"]
+            date_taken = info.find("photo").find("dates").attrib["taken"]
 
-        logger.info("     %s %s" % (date_taken, title))
+            logger.info("     %s %s", date_taken, title)
 
-        entry_store.add_entry(
-            type="flickr",
-            id=id,
-            title=title,
-            url=page_url,
-            source="flickr",
-            date=date_taken,
-            image=image,
-        )
+            entry_store.add_entry(
+                type="flickr",
+                id=photo_id,
+                title=title,
+                url=page_url,
+                source="flickr",
+                date=date_taken,
+                image=image,
+            )
 
-dbcxn.close()
+    dbcxn.close()
+
+
+if __name__ == "__main__":
+    main()
